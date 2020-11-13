d911d712e3c4e1142573746271ffd1997ebfad1d
wlkz <wikizhu@outlook.com>
2020-11-13 16:44:01 +0800

    fix: https://greasyfork.org/zh-CN/scripts/408547-%E8%87%AA%E5%8A%A8%E8%B7%B3%E8%BD%AC%E5%BE%AE%E5%8D%9A%E7%9F%AD%E9%93%BE%E6%8E%A5/discussions/64074

--- fuck-weibo-redirect-block.meta.js
+++ fuck-weibo-redirect-block.meta.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         自动跳转微博短链接
-// @version      0.1.1
+// @version      0.1.2
 // @author       wlkz
 // @description  fuck weibo url shorter service (t.cn) redirect blocking page, and redirect to target page automatically.
 // @homepage     https://github.com/wlkz/userscript-fuck-weibo-redirect-block
 // @supportURL   https://github.com/wlkz/userscript-fuck-weibo-redirect-block/issues
 // @match        *://t.cn/*
+// @match        *://weibo.cn/sinaurl?*
 // @grant        none
 // @run-at       document-end
 // @license MIT
